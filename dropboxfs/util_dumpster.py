0b02d5bf36c7212692d70ba2229b10c099ff3bac
Rian Hunter <rian@alum.mit.edu>
2016-04-29 10:25:33 -0700

    Add PositionIO

--- dropboxfs/util_dumpster.py
+++ dropboxfs/util_dumpster.py
@@ -5,8 +5,42 @@
 # keep this file small, if there is a theme of utility functions then
 # put group them into a separate file
 
+import io
 import datetime
+import threading
 
 def utctimestamp(dt):
     assert dt.tzinfo is None
     return dt.replace(tzinfo=datetime.timezone.utc).timestamp()
+
+class PositionIO(io.RawIOBase):
+    def __init__(self):
+        self._offset_lock = threading.Lock()
+        self._offset = 0
+
+    def readinto(self, ibuf):
+        with self._offset_lock:
+            obuf = self.pread(self._offset, len(ibuf))
+            ibuf[:len(obuf)] = obuf
+            self._offset += len(obuf)
+            return len(obuf)
+
+    def write(self, buf):
+        with self._offset_lock:
+            ret = self.pwrite(buf, self._offset)
+            self._offset += ret
+            return ret
+
+    def seek(self, amt, whence=0):
+        with self._offset_lock:
+            if whence == io.SEEK_SET:
+                self._offset = amt
+            elif whence == io.SEEK_CUR:
+                self._offset += amt
+            elif whence == io.SEEK_END:
+                self._offset = self._file_length() + amt
+            else:
+                raise OSError(errno.EINVAL, os.strerror(errno.EINVAL))
+
+    def seekable(self):
+        return hasattr(self, '_file_length')

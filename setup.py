683d6cefd5113de5ba65b660e353032474f76524
Rian Hunter <rian@alum.mit.edu>
2018-10-08 12:34:12 -0700

    Release 1.0.19

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="dbxfs",
-    version='1.0.18',
+    version='1.0.19',
     author="Rian Hunter",
     author_email="rian@alum.mit.edu",
     description="User-space file system for Dropbox",
